dc49dd352a73572f07b584109020270aac464eb1
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-04-19 10:41:47 -0600

    Add Materialize to Project:
    Add cdn for both javascript and css to application.html.erb
    add grid system for project
    add navbar to top for all pages
    dynamic resizing of images to screen

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+category = Category.create(name: "Haikus")
+item_1 = Item.create(title: "item 1",
+                     description: "This is the first item",
+                     price: 9.99,
+                     image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png",
+                     category_id: category.id)
+item_2 = Item.create(title: "item 2",
+                     description: "This is the second item",
+                     price: 5.99,
+                     image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+item_3 = Item.create(title: "item 3",
+                     description: "This is the second item",
+                     price: 5.99,
+                     image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+item_4 = Item.create(title: "item 4",
+                     description: "This is the second item",
+                     price: 5.99,
+                     image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+item_5 = Item.create(title: "item 5",
+                     description: "This is the second item",
+                     price: 5.99,
+                     image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+item_6 = Item.create(title: "item 6",
+                     description: "This is the second item",
+                     price: 5.99,
+                     image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+item_7 = Item.create(title: "item 7",
+                     description: "This is the second item",
+                     price: 5.99,
+                     image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
