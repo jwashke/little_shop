302f6a0f70df0329a19eb376c23db2c91bd47f51
Josh Washke <jwashke@icloud.com>
2016-04-18 18:24:41 -0600

    Implement browse items by category
    When visiting a category page, a user sees all items associated with that category

---
+++ spec/features/user_can_browse_items_by_category_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.feature "User can view category index" do
+  scenario "they see the items sorted by category" do
+    category = Category.create(name: "Haikus")
+    item_1 = Item.create(title: "item 1",
+                         description: "This is the first item",
+                         price: 9.99,
+                         image_path: "example.image",
+                         category_id: category.id)
+    item_2 = Item.create(title: "item 2",
+                         description: "This is the second item",
+                         price: 5.99,
+                         image_path: "example.image/2")
+
+    visit category_path(category.name)
+
+    expect(page).to have_content("item 1")
+    expect(page).to have_content("9.99")
+    expect(page).to have_css("img[src=\"#{item_1.image_path}\"]")
+
+    expect(page).not_to have_content("item 2")
+    expect(page).not_to have_content("5.99")
+    expect(page).not_to have_css("img[src=\"#{item_2.image_path}\"]")
+  end
+end
