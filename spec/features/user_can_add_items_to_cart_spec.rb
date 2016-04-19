065ed70f341f81bde324b61a237054c7aa28b289
Josh Washke <jwashke@icloud.com>
2016-04-19 15:02:34 -0600

    Implement feature test user can add items to cart

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -1,16 +1,11 @@
-
-#       I should see a link or button for "Add to Cart"
-#       When I click "Add to cart" for that item
-#       And I click a link or button to view cart
-#       And my current path should be "/cart"
-#       And I should see a small image, title, description and price for the item I just added
-#       And there should be a "total" price for the cart that should be the sum of all items in the cart
-
 require "rails_helper"
 
 RSpec.feature "User can add an item to their cart" do
   scenario 'they see the contents' do
-    item = create_items
+    create_items
+
+    item_1 = Item.first
+    item_2 = Item.last
 
     visit items_path
 
@@ -23,7 +18,7 @@ RSpec.feature "User can add an item to their cart" do
     click_link "Cart"
 
     expect(current_path).to eq(cart_path)
-    save_and_open_page
+
     within("ul.collection:nth-child(1)") do
       expect(page).to have_content("item 1")
       expect(page).to have_content("9.99")
@@ -34,17 +29,19 @@ RSpec.feature "User can add an item to their cart" do
 
     visit items_path
 
-    last(:link, "Add to Cart").click
+    within(".card-#{item_2.id}") do
+      click_button("Add to Cart")
+    end
     click_link "Cart"
 
     expect(current_path).to eq(cart_path)
-    within("ul.collection:nth-child(1)") do
+    within("li.collection-item:nth-child(1)") do
       expect(page).to have_content("item 1")
       expect(page).to have_content("9.99")
       expect(page).to have_content("This is the first item")
       expect(page).to have_css("img[src=\"/images/example.image\"]")
     end
-    within("ul.collection:nth-child(2)") do
+    within("li.collection-item:nth-child(2)") do
       expect(page).to have_content("item 2")
       expect(page).to have_content("5.99")
       expect(page).to have_content("This is the second item")
