0dc42a1942b88ad4c1f1d49d11e81a4f5ef5b097
Josh Washke <jwashke@icloud.com>
2016-04-19 15:21:30 -0600

    Fixing hound complaints

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 RSpec.describe "Cart" do
   it "has initial contents" do
-    cart = Cart.new({ "1" => 1 })
+    cart = Cart.new { "1" => 1 }
 
-    expect(cart.contents).to eq({ "1" => 1 })
+    expect(cart.contents).to eq { "1" => 1 }
   end
 
   it "can add an item" do
-    cart = Cart.new({ "1" => 1 })
+    cart = Cart.new { "1" => 1 }
 
     cart.add_item(1)
     cart.add_item(2)
