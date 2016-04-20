d910ac6428b719a5ddbc563d54fdc7d75934d597
Josh Washke <jwashke@icloud.com>
2016-04-20 17:17:28 -0600

    Implement visitors cart is saved upon account creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       link_to "Login", login_path
     end
   end
+
+  def checkout_or_create_account_button
+    if current_user
+      button_to "Checkout", cart_path, class: "waves-effect waves-light btn"
+    else
+      button_to "Create Account to Checkout", login_path, method: "get", class: "waves-effect waves-light btn"
+    end
+  end
 end
