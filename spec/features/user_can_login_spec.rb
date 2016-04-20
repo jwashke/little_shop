021ec48a4b0f11f3f7d536499a3c8b1dad448399
Josh Washke <jwashke@icloud.com>
2016-04-20 14:56:56 -0600

    12 user can login (#22)
    * Implement Login Form
    
    * User can register a new account
    
    * change routes for login path
    
    * Implement user authentication
    
    User can create an account
    User can login or logout

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.feature "Visitor can visit login page" do
+  scenario "click on create account and see the form to create an account" do
+    visit root_path
+    within(".nav-wrapper") do
+      expect(page).to have_content("Login")
+    end
+
+    click_link("Login")
+
+    expect(current_path).to eq(login_path)
+
+    within("#login-form") do
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Password")
+      expect(page).to have_button("Login")
+    end
+
+    within("#create-form") do
+      expect(page).to have_button("Create Account")
+    end
+  end
+
+  scenario "User can create an account" do
+    visit login_path
+    within("#create-form") do
+      fill_in :Email, with: "user@example.com"
+      fill_in :Password, with: "password"
+      fill_in "First name", with: "Josh"
+      fill_in "Last name", with: "Josh"
+      fill_in :Address, with: "fake address"
+      fill_in :City, with: "Denver"
+      fill_in :State, with: "Colorado"
+      fill_in :Zip, with: "80123"
+    end
+    within("#create-form") do
+      click_button("Create Account")
+    end
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Account successfully created")
+    within(".nav-wrapper") do
+      expect(page).to have_content("Logout")
+      expect(page).not_to have_content("Login")
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:address) }
+  it { is_expected.to validate_presence_of(:city) }
+  it { is_expected.to validate_presence_of(:state) }
+  it { is_expected.to validate_presence_of(:zip) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+end
