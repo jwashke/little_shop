92798a3e7604208cd2f85109b1ad0419678fc11b
Jared Roth <jared.roth@gmail.com>
2016-04-21 11:34:26 -0600

    Maybe hound loves me

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,36 @@ category = Category.create(name: "Haikus")
 Item.create(title: "Item 1",
             description: "This is the first item",
             price: 9.99,
-            image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png",
+            image_path:
+            "http://www.hikashop.com/media/com_hikashop/upload/pdf.png",
             category_id: category.id)
 Item.create(title: "Item 2",
             description: "This is the second item",
             price: 5.99,
-            image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+            image_path:
+            "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
 Item.create(title: "item 3",
             description: "This is the second item",
             price: 5.99,
-            image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+            image_path:
+            "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
 Item.create(title: "item 4",
             description: "This is the second item",
             price: 5.99,
-            image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+            image_path:
+            "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
 Item.create(title: "item 5",
             description: "This is the second item",
             price: 5.99,
-            image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+            image_path:
+            "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
 Item.create(title: "item 6",
             description: "This is the second item",
             price: 5.99,
-            image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+            image_path:
+            "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
 Item.create(title: "item 7",
             description: "This is the second item",
             price: 5.99,
-            image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+            image_path:
+            "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
