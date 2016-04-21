ebdf2ca7ba19f9e6b43cd53d5896a6e9d1215318
Jared Roth <jared.roth@gmail.com>
2016-04-21 11:31:10 -0600

    More Hound complaints

--- spec/features/user_can_browse_items_by_category_spec.rb
+++ spec/features/user_can_browse_items_by_category_spec.rb
@@ -4,15 +4,15 @@ RSpec.feature "User can view category index" do
   scenario "they see the items sorted by category" do
     category = Category.create(name: "Haikus")
     Item.create(title: "Item 1",
-               description: "This is the first item",
-               price: 9.99,
-               image_path: "example.image",
-               category_id: category.id)
+                description: "This is the first item",
+                price: 9.99,
+                image_path: "example.image",
+                category_id: category.id)
 
     Item.create(title: "Item 2",
-               description: "This is the second item",
-               price: 5.99,
-               image_path: "example.image/2")
+                description: "This is the second item",
+                price: 5.99,
+                image_path: "example.image/2")
 
     visit category_path(category.name)
 
