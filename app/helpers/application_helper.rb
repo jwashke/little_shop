fad635dfb246bc254d35bb321d7296703c62e36d
Josh Washke <jwashke@icloud.com>
2016-04-21 11:25:11 -0600

    Implement User can logout (#26)
    * Implement User can logout
    
    * Fix hound complaints

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def link_to_login_or_logout
     if current_user
-      link_to "Logout", logout_path
+      link_to "Logout", logout_path, method: :delete
     else
       link_to "Login", login_path
     end
