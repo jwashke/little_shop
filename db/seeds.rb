ab8250db21051060391cd0a60563ae01a052abfa
Josh Washke <jwashke@icloud.com>
2016-04-21 14:08:40 -0600

    add seed data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,38 @@
-category = Category.create(name: "Haikus")
-category = Category.create(name: "Limericks")
+Category.create(name: "fiction")
+Category.create(name: "literature")
+Category.create(name: "history")
+Category.create(name: "non-fiction")
+
+category_1 = Category.all[0]
+category_2 = Category.all[1]
+category_3 = Category.all[2]
+category_4 = Category.all[3]
+
+
 5.times do |i|
   category_2.items.create(title: "Item #{i}",
                           description: "Description #{i}",
+                          price: 5.99,
                           image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
 end
 
 5.times do |i|
   category_1.items.create(title: "Item #{i + 5}",
                           description: "Description #{i + 5}",
+                          price: 5.99,
+                          image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+end
+
+5.times do |i|
+  category_3.items.create(title: "Item #{i + 10}",
+                          description: "Description #{i + 10}",
+                          price: 5.99,
+                          image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
+end
+
+5.times do |i|
+  category_4.items.create(title: "Item #{i + 15}",
+                          description: "Description #{i + 15}",
+                          price: 5.99,
                           image_path: "http://www.hikashop.com/media/com_hikashop/upload/pdf.png")
 end
