ac562766d4568563e4afdd2291ebbb040af49beb
Josh Washke <jwashke@icloud.com>
2016-04-23 14:03:58 -0600

    fixing hound complaints

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160423035419) do
-=======
-ActiveRecord::Schema.define(version: 20160422144657) do
->>>>>>> 38_retired_items
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
