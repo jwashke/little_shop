34d9adf2dc4219011cfd369305218f017a5a4e78
Jared Roth <jared.roth@gmail.com>
2016-04-24 11:13:32 -0700

    Remove some duplications in factories
    DRY's up a few sub-classes by relocating them into their parent factories
    All tests passing, ready to be merged

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -10,21 +10,16 @@ FactoryGirl.define do
     city "faketown"
     state "CA"
     zip "92504"
+
+    factory :admin do
+      sequence :email do |n|
+        "admin#{n}@example.com"
+      end
+      role 1
+    end
+
   end
 
-  factory :admin, class: :user do
-    sequence :email do |n|
-      "admin#{n}@example.com"
-    end
-    password "password"
-    first_name "Jon"
-    last_name "Doe"
-    address "123 Fake Street"
-    city "faketown"
-    state "CA"
-    zip "92504"
-    role 1
-  end
 
   factory :item do
     sequence :title do |n|
@@ -33,32 +28,31 @@ FactoryGirl.define do
     description "Some item"
     price 9.99
     image_path "example.jpg"
+
+    factory :retired_item do
+      sequence :title do |n|
+        "retired item #{n}"
+      end
+      state 1
+    end
+
   end
 
-  factory :retired_item, class: :item do
-    sequence :title do |n|
-      "retired item #{n}"
-    end
-    description "Some item"
-    price 9.99
-    image_path "example.jpg"
-    state 1
-  end
 
   factory :order do
+    factory :paid_order do
+      status 1
+    end
+
+    factory :cancelled_order do
+      status 2
+    end
+
+    factory :completed_order do
+      status 3
+    end
   end
 
-  factory :paid_order, class: :order do
-    status 1
-  end
-
-  factory :cancelled_order, class: :order do
-    status 2
-  end
-
-  factory :completed_order, class: :order do
-    status 3
-  end
 
   factory :invoice do
     user
