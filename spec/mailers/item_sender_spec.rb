0e112960e86860420192277ce8d045ac3dc4c344
Josh Washke <jwashke@icloud.com>
2016-04-26 15:53:34 -0600

    Fix hound complaints

--- spec/mailers/item_sender_spec.rb
+++ spec/mailers/item_sender_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe ItemSender, type: :mailer do
-
   describe ".send_order" do
     before(:each) do
       @user = create(:user)
@@ -17,16 +16,18 @@ RSpec.describe ItemSender, type: :mailer do
       expect(ActionMailer::Base.deliveries.count).to eq(1)
     end
 
-    it 'has the correct receiver email' do
+    it "has the correct receiver email" do
       expect(ActionMailer::Base.deliveries.first.to).to eq([@user.email])
     end
 
-    it 'has the correct subject' do
-      expect(ActionMailer::Base.deliveries.first.subject).to eq("Here is your Order!")
+    it "has the correct subject" do
+      message = "Here is your Order!"
+      expect(ActionMailer::Base.deliveries.first.subject).to eq(message)
     end
 
-    it 'has the correct sender email' do
-      expect(ActionMailer::Base.deliveries.first.from).to eq(["from@example.com"])
+    it "has the correct sender email" do
+      email = "from@example.com"
+      expect(ActionMailer::Base.deliveries.first.from).to eq([email])
     end
   end
 end
