455d173eae994903ff328dcc4953f7485c9952f7
Josh Washke <jwashke@icloud.com>
2016-05-08 18:19:09 -0600

    Update Routes to use verb/path combo instead of resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root to: "items#index"
 
-  resources :items, only: [:index]
-  resource :cart, only: [:create, :show, :destroy, :update]
-  resource :users, only: [:create]
-  resources :orders, only: [:create, :index, :show]
+  get "/items", to: "items#index"
+  post "/cart", to: "carts#create"
+  get "/cart", to: "carts#show"
+  delete "/cart", to: "carts#destroy"
+  patch "/cart", to: "carts#update"
+  post "/users", to: "users#create"
+  post "/orders", to: "orders#create"
+  get "/orders", to: "orders#index"
+  get "/orders/:id", to: "orders#show", as: :order
 
   get "/login", to: "users#new"
   post "/login", to: "sessions#create"
@@ -13,15 +18,19 @@ Rails.application.routes.draw do
   get "/auth/github/callback", to: "sessions#create"
   get "/dashboard", to: "orders#index"
 
-  namespace :admin do
-    get "/dashboard", to: "dashboard#show"
-    patch "/items/:id/retire", to: "items#retire", as: :retire
-    patch "/items/:id/activate", to: "items#activate", as: :activate
-    patch "/orders/:id/cancel", to: "orders#cancel", as: :cancel
-    resources :items, only: [:index, :new, :create, :edit, :update]
-    resources :orders, only: [:index, :update]
-    resources :categories, only: [:new, :create]
-  end
+  get "/admin/dashboard", to: "admin/dashboard#show"
+  patch "/admin/items/:id/retire", to: "admin/items#retire", as: :admin_retire
+  patch "/items/:id/activate", to: "admin/items#activate", as: :admin_activate
+  patch "/admin/orders/:id/cancel", to: "admin/orders#cancel", as: :admin_cancel
+  get "admin/items", to: "admin/items#index"
+  get "admin/items/new", to: "admin/items#new", as: :new_admin_item
+  post "admin/items", to: "admin/items#create"
+  get "admin/items/:id/edit", to: "admin/items#edit", as: :edit_admin_item
+  patch "admin/items/:id", to: "admin/items#update", as: :admin_item
+  get "admin/orders", to: "admin/orders#index", as: :admin_orders
+  patch "admin/orders/:id", to: "admin/orders#update", as: :admin_order
+  get "admin/categories/new", to: "admin/categories#new", as: :new_admin_category
+  post "admin/categories", to: "admin/categories#create"
 
   get "/:name", to: "categories#show", as: :category
 end
